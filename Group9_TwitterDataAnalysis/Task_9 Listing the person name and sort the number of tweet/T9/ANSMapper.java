1639eb24f366823bbf5252e6289bb013d68275d6
Khaleef <khaleefms@gmail.com>
2016-04-04 03:44:38 +0800

    Task_9 Listing the person name and sort the number of tweet

---
+++ Group9_TwitterDataAnalysis/Task_9 Listing the person name and sort the number of tweet/T9/ANSMapper.java
@@ -0,0 +1,47 @@
+import java.util.Hashtable;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class ANSMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	Hashtable<String, String> countryCodes = new Hashtable<>();
+
+	@Override
+	protected void setup(Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// We will put the ISO-3166-alpha3.tsv to Distributed Cache in the
+		// driver class
+		// so we can access to it here locally by its name
+		BufferedReader br = new BufferedReader(new FileReader("ISO-3166-alpha3.tsv"));
+		String line = null;
+		while (true) {
+			line = br.readLine();
+			if (line != null) {
+				String parts[] = line.split("\t");
+				countryCodes.put(parts[0], parts[1]);
+			} else {
+				break;// finished reading
+			}
+		}
+		br.close();
+	}
+
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		// splitting all column
+		String[] parts = value.toString().split(",");
+
+		// taking the name column
+		String ip = parts[18];
+
+		if (ip != null) {
+			context.write(new Text(ip), new IntWritable(1));
+		}
+	}
+}
